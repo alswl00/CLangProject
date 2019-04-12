1711cb50d944d43c3d630ef6addab060a6aa7aac
Minji Kim <s2018s24@e-mirim.hs.kr>
2019-04-12 15:25:30 +0900

    190412

---
+++ class03.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+	int nHakbun;
+	const char* sName;
+public:
+	Student();
+	Student(int, const char*);
+	void show();
+};
+
+Student::Student() {
+	nHakbun = 1234;
+	sName = "이사랑";
+	cout << "학번이 등록되었습니다." << endl;
+}
+
+Student::Student(int nHakbun, const char* sName) {
+	this->nHakbun=nHakbun;
+	this->sName=sName;
+}
+
+void Student::show() {
+	cout << "학번은 " << nHakbun << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl;
+}
+int main() {
+	Student student1;
+	Student student2(5678, "마리아");
+	student1.show();
+	student2.show();
+
+	return 0;//소멸자가 없을 경우에는 컴파일러가 기본소멸자 생성해서 호출
+}
---
+++ class04.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+	int nHakbun;
+	const char* sName;
+public:
+	Student();
+	Student(int, const char*);
+	void show();
+};
+
+Student::Student() {
+	nHakbun = 1234;
+	sName = "이사랑";
+	cout << "학번이 등록되었습니다." << endl;
+}
+
+Student::Student(int nHakbun, const char* sName) {
+	this->nHakbun = nHakbun;
+	this->sName = sName;
+}
+
+void Student::show() {
+	cout << "학번은 " << nHakbun << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl;
+}
+int main() {
+	Student students[3] = {
+		Student(),
+		Student(4567, "하와"),
+		Student(9876,"아담")
+	};
+	for (int i = 0; i < 3; i++)
+		students[i].show();
+
+	return 0;//소멸자가 없을 경우에는 컴파일러가 기본소멸자 생성해서 호출
+}
