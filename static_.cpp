4311c41229e1497dce8a1e7427f64905efd9b927
Minji Kim <s2018s24@e-mirim.hs.kr>
2019-04-24 22:51:59 +0900

    2학년 C++실습

---
+++ static_.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+void jungjuk() {
+	static int a = 0;
+	int b = 0;
+	cout << "정적 변수 a는" << a << ",지역변수b:" << b << endl;
+	a++; b++;
+}
+int main() {
+	jungjuk();
+	jungjuk();
+	jungjuk();
+
+	return 0;
+}
